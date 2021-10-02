8d5893df065c0b8aab243006b9cb7736d6584eb2
Daniel Vazquez <dnlvazquez98@gmail.com>
2021-10-02 16:10:44 -0500

    Delete Homework1 directory

--- Homework1/hw_01_02_Vazquez_v1.00.py
+++
@@ -1,8 +0,0 @@
-------------------------------------------------------------------------------80
-    #Program: Hello World
-     #Author: Daniel Vazquez-Pedroza
-       #Date: 1/14/2017
-
-  #Objective: Say "Hello World."
-
-print("hello world")
