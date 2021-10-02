3e448efdc1905ee859c55120bba2cc4e1356e21f
Daniel Vazquez <dnlvazquez98@gmail.com>
2021-10-02 16:10:51 -0500

    Add files via upload

---
+++ Homework 1/hw_01_02_Vazquez_v1.00.py
@@ -0,0 +1,8 @@
+------------------------------------------------------------------------------80
+    #Program: Hello World
+     #Author: Daniel Vazquez-Pedroza
+       #Date: 1/14/2017
+
+  #Objective: Say "Hello World."
+
+print("hello world")
---
+++ Homework 1/hw_01_03_Vazquez_v1.00.py
@@ -0,0 +1,15 @@
+-----------------------------------------------------------------------------80
+    #Program: Name
+     #Author: Daniel Vazquez-Pedroza
+       #Date: 1.14.2017
+
+  #Objective: Ask for the user for his/her name. Maybe some small talk as well.
+
+Name = input("User's name: ")
+print("Welcome")
+print(Name)
+input("How are you doing today? ")
+print("Would you like a cup of coffee or a picture of a kitty?")
+input("...or both? ")
+print("I'm sorry, but I don't have the sick skills to import images yet.")
+exit = input("Come back later. ")
