e764c186a83f3522692aae55301ec51ca965c079
Daniel Vazquez <dnlvazquez98@gmail.com>
2021-10-02 16:08:41 -0500

    Add files via upload

---
+++ Homework1/hw_01_04_Vazquez_v1.00.py
@@ -0,0 +1,15 @@
+------------------------------------------------------------------------------80
+    #Program: Area of a Circle
+     #Author: Daniel Vazquez-Pedroza
+       #Date: 1.14.2017
+
+  #Objective: Write a program that can sove the area of a circle with the
+             #radius of said circle being provided by the user.
+
+print("So you want to calculate the area of a circle, eh? Easy.")
+X = float(input("What is the radius of that circle? "))
+print (3.14159*X*X)
+print("You're welcome ;)")
+
+#http://stackoverflow.com/questions/12570937/python-typeerror-cant-multiply-sequence-by-non-int-of-type-float
+#That is where I got my explanation for the float(input(
